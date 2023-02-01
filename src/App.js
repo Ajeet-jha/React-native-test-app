428b073398f3f9f8993c7735c130c008e0d042df
Ajeet kumar jha <49264063+Ajeet-jha@users.noreply.github.com>
2023-02-01 10:25:25 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function App() {
+  return (
+    <View style={styles.app}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
